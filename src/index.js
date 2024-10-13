e3d960548b7917a1de57c809ddcf8734b79fcceb
samanthadsilver <154371353+samanthadsilver@users.noreply.github.com>
2024-10-13 17:14:04 +0100

    Updated user experience
    Updated user experience

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function displayAdvice(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
-    delay: 10,
+    delay: 20,
   });
 }
 
@@ -13,9 +13,13 @@ function generateAdvice(event) {
   let instructionsInput = document.querySelector("#location");
   let apiKey = "dd9ad0073b1c71eoafbt4187fa1bef0d";
   let context =
-    "You are a very well-travelled AI and have been to every country in the world. Please give eight recommendation on things to see and do and write this in basic HTML with the headline of the suggestions in <h2> and then further instructions below as body text. Please follow the user instructions";
+    "You are a very well-travelled AI and have been to every country in the world. Please give eight recommendation on things to see and do and write this in basic HTML. Include a title in <h1> and include each suggestion below with the headline of the suggestions in <h2> and then further instructions below as body text. Do not put ```html at the beginning or ``` at the end. Please follow the user instructions";
   let prompt = `User intructions: Generate recommendations on travelling to ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let adviceElement = document.querySelector("#advice");
+  adviceElement.classList.remove("hidden");
+  adviceElement.innerHTML = `<div class="generating"> ⌛ Generating Travel Advice about ${instructionsInput.value}</div>`;
 
   console.log("Generating advice");
   console.log(`Prompt: ${prompt}`);
